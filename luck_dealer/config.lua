1cd5167438e4ad6b1217550254cbbe3952fcd148
doğu <42766561+goodluckgod@users.noreply.github.com>
2020-08-29 17:53:16 +0300

    Add files via upload

---
+++ luck_dealer/config.lua
@@ -0,0 +1,13 @@
+Config = {}
+
+Config.Locale = 'tr'
+
+Config.Allrounditems = {
+	fish = 60,
+}
+
+Config.Log = "dc" -- Seçenekler: dc (Webhook gerektirir, discorda gönderir), console (server konsoluna printler), false (log tutmaz).
+
+Config.CircleZones = {
+	Allrounddealer = {coords = vector3(-266.21,-961.64,31.25), name = _U('blip_allrounddealer'), color = 2, sprite = 222, radius = 125.0}
+}
